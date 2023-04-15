103d074dc3c715def0f7bb72e24c489e65e1e26e
PabloForero <124003973+PabloForero@users.noreply.github.com>
2023-04-14 20:28:11 -0400

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Testing.Test1;
+
+/**
+ *
+ * @author pablo
+ */
+public class test1 {
+    public static void main(String args[])
+    {
+        int i=1;  // delare 
+        do
+        {
+            System.out.println(i);
+            i++;
+        }
+        while(i<=5);
+    }
+}
---
+++ test2.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Testing.Test1;
+
+/**
+ *
+ * @author pablo
+ */
+
+   public class test2 {
+    private String name;
+    private int age;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
